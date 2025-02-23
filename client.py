c615384b104220128ea0cbc59eb43b9eb394bcdc
codeurge123 <bansalyash316@gmail.com>
2025-02-23 19:23:09 +0530

    Alexa assistant

---
+++ client.py
@@ -0,0 +1,15 @@
+# from openai import OpenAI
+
+# client = OpenAI(
+#     api_key = "https:/v1/organization/users",
+# )
+
+# completion = client.chat.completions.create(  
+#     model = "gpt-3.5-turbo",
+#     message = [
+#         {"role": "system","content": "You are a virtual assistent named jarvis skilled tasks like Alexa Google Cloud"},
+#         {"role": "user","content": "what is coding"}
+#     ]
+# )
+
+# print(completion.choices[0].message)
---
+++ musiclibrary.py
@@ -0,0 +1,5 @@
+music = {
+    "aura": "https://www.youtube.com/watch?v=bsvmj3521Vs",
+    "thugle": "https://www.youtube.com/watch?v=LEYXdZ_rVbo",
+    "goat": "https://www.youtube.com/watch?v=cl0a3i2wFcc"
+}
